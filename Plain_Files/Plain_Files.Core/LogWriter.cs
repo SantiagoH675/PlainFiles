48040d5a0dcdf70aab69e563a45032cb8bc789cd
SANTIAGO HERRERA MUNERA <santiagoherrera1141378@correo.itm.edu.co>
2025-11-18 19:57:32 -0500

    install person

---
+++ Plain_Files/Plain_Files.Core/LogWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plain_Files.Core;
+
+public class LogWriter : IDisposable
+{
+    private readonly StreamWriter _writer;
+
+    public LogWriter(string path)
+    {
+        _writer = new StreamWriter(path, append: true)
+        {
+            AutoFlush = true
+        };
+    }
+
+    public void WriteLog(string level, string message)
+    {
+        var timestamp = DateTime.Now.ToString("s");
+        _writer.WriteLine($"[{timestamp}] - [{level}]-[{message}]");
+    }
+
+    public void Dispose()
+    {
+        _writer?.Dispose();
+    }
+}
